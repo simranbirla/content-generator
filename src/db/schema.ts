c715b2c1f10b29b57e2df8535617a8c823c1687c
simranbirla <simranbirla@Simrans-MacBook-Pro.local>
2024-12-05 13:15:37 +0530

    feat: db connection

---
+++ src/db/schema.ts
@@ -0,0 +1,9 @@
+import { integer, pgTable, text, varchar } from "drizzle-orm/pg-core";
+
+export const Users = pgTable("users", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  age: integer().notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+  displayPictureUrl: text()
+});
