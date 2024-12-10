5c6b61d7748f1a58f295829ec1da53c15b8a5f81
simranbirla <simranbirla@Simrans-MacBook-Pro.local>
2024-12-10 17:42:16 +0530

    chore: header sign out

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,17 +1,19 @@
 'use client'
 
 import React, { useContext } from 'react'
-import { BookOpenText } from 'lucide-react';
+import { BookOpenText, LogOut } from 'lucide-react';
 import Link from 'next/link';
 import { Button } from "@/components/ui/button"
 import { UserContext } from '@/context/UserContext';
 import { Avatar, AvatarImage } from '@/components/ui/avatar';
-import { signInWithPopup } from 'firebase/auth';
+import { signInWithPopup, signOut } from 'firebase/auth';
 import { auth, provider } from '@/firebase'
+import { DropdownMenu, DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu';
+import { DropdownMenuContent, DropdownMenuItem } from '@/components/ui/dropdown-menu';
 
 export default function Header() {
 
-    const { user, login } = useContext(UserContext);
+    const { user, login, logout } = useContext(UserContext);
 
     const signIn = async () => {
         const result = await signInWithPopup(auth, provider)
@@ -20,6 +22,12 @@ export default function Header() {
             name: result.user.displayName,
             email: result.user.email,
         })
+    }
+
+    const loggingOut = async () => {
+        await signOut(auth)
+        logout()
+
     }
 
 
@@ -32,9 +40,26 @@ export default function Header() {
             <nav className='flex gap-10 items-center'>
                 <Link href={'/generate'}>Generate</Link>
                 <Link href={'/github'}>Github</Link>
-                {user ? <Avatar>
-                    <AvatarImage src={user.displayPictureUrl} />
-                </Avatar> : <Button onClick={signIn}>Sign in</Button>}
+                {user ?
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Avatar>
+                                <AvatarImage src={user.displayPictureUrl} />
+                            </Avatar>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent className="w-56">
+                            <DropdownMenuItem>
+                                <span>{user.name}</span>
+                            </DropdownMenuItem>
+                            <DropdownMenuItem onClick={loggingOut}>
+                                <LogOut />
+                                <span>Log out</span>
+
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+
+                    : <Button onClick={signIn}>Sign in</Button>}
             </nav>
         </header>
     )
