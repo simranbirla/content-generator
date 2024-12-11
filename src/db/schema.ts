9721f6e12d734e423a5abd21380d7227ee0d1f5f
simranbirla <simranbirla@Simrans-MacBook-Pro.local>
2024-12-11 16:24:34 +0530

    chore: posts schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,17 @@
-import { integer, pgTable, text, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, text, timestamp, varchar } from "drizzle-orm/pg-core";
 
 export const Users = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
-  displayPictureUrl: text()
+  displayPictureUrl: text(),
 });
+
+
+export const Posts = pgTable("posts", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  text: text(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  userId: integer('user_id').references(() => Users.id).notNull(),
+  displayPictureUrl: text(),
+})
